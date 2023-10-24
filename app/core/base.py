73ae98df093c3a0ddca3cd97e71eb022223ffc3f
Alexandr <pokertrend@yandex.ru>
2023-10-24 21:30:22 +0400

    Remove ignored files

--- app/core/base.py
+++
@@ -1,2 +0,0 @@
-from app.core.db import Base # noqa
-from app.models import User, Donation, CharityProject # noqa
--- app/crud/donation.py
+++
@@ -1,9 +0,0 @@
-from app.crud.base import CRUDBase
-from app.models.donation import Donation
-
-
-class DonationProject(CRUDBase):
-    """CRUD класс фонда"""
-
-
-donation_crud = DonationProject(Donation)
